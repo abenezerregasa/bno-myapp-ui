80ab483646297f747db84a6be359eaa5eb70c25a
abenezerregasa <abenezerregasa@gmail.com>
2024-10-24 22:39:18 +0200

    added

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+ 
+ 
+
 export default defineConfig({
+  root: './client/client',  // This points Vite to the correct folder
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
 });
+
+
+ 
